95abc93a17fd57e66c83567ef5bdfa8c44e70943
Yiyi <414956851@qq.com>
2019-02-17 23:01:13 -0500

    Replaced cube with virusbot, adjusted light setting and control

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LightSwitch : MonoBehaviour
+{
+    private Light myLight;
+
+    void Start() { 
+    myLight = GetComponent<Light>();
+}
+    public void ToggleLight()
+    {
+        myLight.enabled = !myLight.enabled;
+    }
+}
